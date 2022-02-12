52fb79bd3758c4ab76fa710b54045b011c264474
Will <willmdbaker@gmail.com>
2022-02-12 21:35:10 +0000

    Added Text

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,38 @@
 <template>
+    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 873 400" xml:space="preserve" class="title">
+        <path class="letter-r" data-letter="r" fill="#7A30CF" d="M871.6,305.1	c-4.3-14.8-17.9-25.1-33.3-25.1h-53.9v120h19.2v-50.4h23.7l24.3,50.4H873l-25.1-51.7C866.3,342.9,877,323.6,871.6,305.1z M803.5,330.4v-31.2h34.7c8.6,0,15.6,7,15.6,15.6c0,8.6-7,15.6-15.6,15.6H803.5z"/>
+        <path class="letter-e" data-letter="e" fill="#7A30CF" d="M688.6,280h80v19.2h-60.8v31.2h56	v19.2h-56v31.2h60.8V400h-80V280z"/>
+        <path class="letter-p" data-letter="p" fill="#7A30CF" d="M641.3,280h-53.9v120h19.2v-50.4h34.7c19.2,0,34.8-15.6,34.8-34.8	C676.1,295.6,660.5,280,641.3,280z M641.3,330.4h-34.7v-31.2h34.7c8.6,0,15.6,7,15.6,15.6C656.9,323.4,649.9,330.4,641.3,330.4z"/>
+        <path class="letter-o" data-letter="o" fill="#7A30CF" d="M526.9,280c-24.6,0-44.5,19.9-44.5,44.5c0,0,0,0,0,0v31c0.2,24.5,20,44.3,44.5,44.5	c24.5-0.2,44.3-20,44.5-44.5v-31C571.4,299.9,551.5,280,526.9,280z M552.2,355.5c0,14-11.3,25.3-25.3,25.3	c-14,0-25.3-11.3-25.3-25.3l0,0v-31c0-14,11.3-25.3,25.3-25.3c14,0,25.3,11.3,25.3,25.3V355.5z"/>
+        <path class="letter-l" data-letter="l" fill="#7A30CF" d="M425.1,380.8h51.2V400h-70.4l0-120	h19.2V380.8z"/>
+        <path class="letter-e" data-letter="e" fill="#7A30CF" d="M310.2,280h80v19.2h-60.8v31.2h56	v19.2h-56v31.2h60.8V400h-80V280z"/>
+        <path class="letter-v" data-letter="v" fill="#7A30CF" d="M279.7,280h20.2l-39.2,120h-25	l-39.4-120h20.2l31.7,96.6L279.7,280z"/>
+        <path class="letter-e" data-letter="e" fill="#7A30CF" d="M109.4,280h80v19.2h-60.8v31.2h56	v19.2h-56v31.2h60.8V400h-80V280z"/>
+        <path class="letter-d" data-letter="d" fill="#7A30CF" d="M49,280H4.5v120H49c24.5-0.2,44.3-20,44.5-44.5v-31C93.3,300,73.5,280.2,49,280z M74.2,355.5c0,13.9-11.3,25.3-25.3,25.3H23.7v-81.6H49c13.9,0,25.3,11.3,25.3,25.3V355.5z"/>
+        <path class="letter-d" data-letter="d" fill="#7A30CF" d="M847,215.5v-31	c-0.2-24.5-20-44.3-44.5-44.5h-44.5l0,120h44.5C827.1,259.8,846.9,240,847,215.5z M777.3,159.2h25.3c13.9,0,25.3,11.3,25.3,25.3v31	c0,13.9-11.3,25.3-25.3,25.3h-25.3V159.2z"/>
+        <path class="letter-n" data-letter="n" fill="#7A30CF" d="M669.1,260h-19.2V140h21.4	l48.3,85.1V140h19.2v120h-21.4l-48.3-85.1V260z"/>
+        <path class="letter-e" data-letter="e" fill="#7A30CF" d="M634.2,260h-80V140h80v19.2h-60.8	v31.2h56v19.2h-56v31.2h60.8V260z"/>
+        <path class="letter-dash" data-letter="dash" fill="#7A30CF" d="M484.5,205.6h51.2v19.2h-51.2	L484.5,205.6z"/>
+        <path class="letter-t" data-letter="t" fill="#7A30CF" d="M401.4,140h80v19.2H451V260h-19.2	V159.2h-30.4V140z"/>
+        <path class="letter-n" data-letter="n" fill="#7A30CF" d="M371.8,140H391v120h-21.4	l-48.3-85.1V260h-19.2V140h21.4l48.3,85.1V140z"/>
+        <path class="letter-o" data-letter="o" fill="#7A30CF" d="M241.6,260	c24.5-0.2,44.3-20,44.5-44.5v-31c0-24.6-19.9-44.5-44.5-44.5c-24.6,0-44.5,19.9-44.5,44.5v31C197.3,240,217.1,259.8,241.6,260z M216.3,215.5v-31c0-14,11.3-25.3,25.3-25.3c14,0,25.3,11.3,25.3,25.3c0,0,0,0,0,0v31c0,14-11.3,25.3-25.3,25.3	C227.6,240.8,216.3,229.5,216.3,215.5C216.3,215.5,216.3,215.5,216.3,215.5z"/>
+        <path class="letter-r" data-letter="r" fill="#7A30CF" d="M116.8,209.6h23.7l24.3,50.4h21.4l-25.1-51.7c18.5-5.4,29.1-24.7,23.7-43.2	c-4.3-14.8-17.9-25.1-33.3-25.1H97.6v120h19.2V209.6z M116.8,159.2h34.7c8.6,0,15.6,7,15.6,15.6c0,8.6-7,15.6-15.6,15.6h-34.7V159.2	z"/>
+        <path class="letter-f" data-letter="f" fill="#7A30CF" d="M23.7,159.2v31.2h56v19.2h-56V260	H4.5V140h80v19.2H23.7z"/>
+        <path class="letter-r" data-letter="r" fill="#7A30CF" d="M467,69.6h23.7L515,120h21.4	l-25.1-51.7c14.9-4.3,25.2-18,25.1-33.6C536.5,15.6,521,0,501.8,0c0,0,0,0-0.1,0h-53.9v120H467V69.6z M467,19.2h34.7	c8.5,0.1,15.5,7,15.5,15.5c0,8.6-6.9,15.6-15.5,15.7H467V19.2z"/>
+        <path class="letter-o" data-letter="o" fill="#7A30CF" d="M387.4,120	c24.5-0.2,44.3-20,44.5-44.5v-31C431.7,20,411.9,0.2,387.4,0c-24.5,0.2-44.3,20-44.5,44.5v31C343.1,100,362.9,119.8,387.4,120z M362.1,44.5c0-13.9,11.3-25.3,25.3-25.3c13.9,0,25.3,11.3,25.3,25.3v31c0,13.9-11.3,25.3-25.3,25.3c-13.9,0-25.3-11.3-25.3-25.3	V44.5z"/>
+        <path class="letter-i" data-letter="i" fill="#7A30CF" d="M326.7,120h-19.2V0h19.2V120z"/>
+        <path class="letter-n" data-letter="n" fill="#7A30CF" d="M218.6,120h-19.2V0h21.4l48.3,85.1	V0h19.2v120h-21.4l-48.3-85.1V120z"/>
+        <path class="letter-e" data-letter="e" fill="#7A30CF" d="M183.7,120h-80V0h80v19.2h-60.8	v31.2h56v19.2h-56v31.2h60.8V120z"/>
+        <path class="letter-s" data-letter="s" fill="#7A30CF" d="M69.8,85.1	c-0.1-8.6-7.1-15.5-15.7-15.5H34.9c-0.1,0-0.2,0-0.2,0C15.4,69.5-0.1,53.9,0,34.7c0-0.1,0-0.1,0-0.2C0.1,15.4,15.7-0.1,34.9,0h20.5	C70.9-0.1,84.6,10.2,89,25.1l-18.4,5.3c-2.1-6.6-8.2-11.2-15.2-11.2H34.9c-8.6,0-15.6,6.9-15.7,15.5c0,8.7,7,15.7,15.7,15.7h19.2	c0.1,0,0.1,0,0.2,0C73.4,50.4,89,65.9,89,85.1c0,0.1,0,0.2,0,0.2c-0.1,19.2-15.7,34.7-34.9,34.6H33.6C18,120.1,4.3,109.8,0,94.9	l18.4-5.3c2.1,6.6,8.2,11.2,15.2,11.2h20.5C62.7,100.8,69.7,93.8,69.8,85.1z"/>
+    </svg>
     <div ref="physicsArea" id="physics-header"></div>
 </template>
 
 <script>
 
 import Matter from 'matter-js'
+
+// this.render = require('../render')
 
 export default {
     name: 'Header',
@@ -16,18 +44,16 @@ export default {
                 Runner = Matter.Runner,
                 Bodies = Matter.Bodies,
                 Composite = Matter.Composite,
-                Composites = Matter.Composites,
-                Common = Matter.Common,
+                // Composites = Matter.Composites,
+                // Common = Matter.Common,
                 MouseConstraint = Matter.MouseConstraint,
                 Mouse = Matter.Mouse
 
-            // create an engine
-            var engine = Engine.create();
+            this.engine = Engine.create();
 
-            // create a renderer
             this.render = Render.create({
                 element: this.$refs.physicsArea,
-                engine: engine,
+                engine: this.engine,
                 options: {
                     width: window.innerWidth,
                     height: window.innerHeight,
@@ -35,18 +61,13 @@ export default {
                     wireframes: false,
                 }
             });
+            this.title = document.querySelector('.title')   
+            this.paths = this.title.querySelectorAll('path')
 
-            // create two boxes and a ground
-            var boxA = Bodies.rectangle(400, 200, 80, 80);
-            var boxB = Bodies.rectangle(450, 50, 80, 80);
-            var stack = Composites.stack(window.innerWidth/4, 0, 10, 10, 1, 15, function(x, y) {
-                return Bodies.circle(x, y, Common.random(15, 30), { restitution: 0.6, friction: 0.1 });
-            }); 
             var ground = Bodies.rectangle(window.innerWidth/2, window.innerHeight, window.innerWidth, 60, { isStatic: true });
             
-
             var mouse = Mouse.create(this.render.canvas)
-            var mouseConstraint = MouseConstraint.create(engine, {
+            var mouseConstraint = MouseConstraint.create(this.engine, {
                 mouse: mouse,
                 constraint: {
                     stiffness: 0.2,
@@ -55,11 +76,15 @@ export default {
                     }
                 }
             });
+            this.addLetters()
 
+            console.log(this.engine.world)
+
+
+            Composite.add(this.engine.world, [ground]);
             // add all of the bodies to the world
-            Composite.add(engine.world, [boxA, boxB, ground, stack]);
 
-            Composite.add(engine.world, mouseConstraint);
+            Composite.add(this.engine.world, mouseConstraint);
 
             // run the renderer
             Render.run(this.render);
@@ -68,14 +93,57 @@ export default {
             var runner = Runner.create();
 
             // run the engine
-            Runner.run(runner, engine);
+            Runner.run(runner, this.engine);
             
         },
+
+        addLetters () {
+            this.paths.forEach(path => {
+            const letter = path.dataset.letter
+            const bounding = path.getBoundingClientRect()
+            
+            const rectangle = Matter.Bodies.rectangle(
+                bounding.left + bounding.width / 2, 
+                bounding.top + bounding.height / 2, 
+                bounding.width,
+                bounding.height + 20,
+                {
+                // isSleeping: true,
+                collisionFilter: {
+                    category: 0x0004
+                },
+                render: {
+                    sprite: {
+                    texture: 'https://assets.codepen.io/5817405/letter-' + letter + '.svg'
+                    }
+                }
+                }
+            )
+            
+            Matter.Composite.add(this.engine.world, rectangle)
+            })
+        },
+
+    
+        windowResizeHandler: function () {
+            this.render.options.width =  window.innerWidth
+            this.render.options.height = window.innerHeight
+        }
     },
+
+
+
+
     mounted: function() {
 
         this.init()
-    }
+    },
+    created() {
+        window.addEventListener("resize", this.windowResizeHandler);
+    },
+    unmounted() {
+        window.removeEventListener("resize", this.windowResizeHandler);
+    },
 
 }
 </script>
@@ -84,6 +152,20 @@ export default {
 
 #physics-header {
     overflow: clip;
+}
+
+.title {
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50px;
+  width: 873px;
+  height: auto;
+  
+  opacity: 0;
+  pointer-events: none;
+  transform: translate3d(0, -50%, 0);
+  z-index: 2;
 }
 
 </style>
