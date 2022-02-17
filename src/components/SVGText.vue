4dde856a1b33c3584c3464e9550583575b778535
Will <willmdbaker@gmail.com>
2022-02-17 13:56:51 +0000

    Got it to draw the letters

--- src/components/SVGText.vue
+++ src/components/SVGText.vue
@@ -1,5 +1,8 @@
 <template>
     <h1>{{ text }}</h1>
+    <svg width="100px" height="100px" viewBox="0 0 4000 2000" xmlns="http://www.w3.org/2000/svg" class="svg-area">
+
+    </svg>
 </template>
 <script>
 
@@ -18,16 +21,40 @@ export default {
                 .then(response => response.text())
                 .then((text) => {
                     const nodes = parser.parseFromString(text, "image/svg+xml").documentElement.childNodes
-                    nodes.forEach(node => {
-                        if(node.attributes){
+                    const svg = document.getElementsByTagName('svg')[0]
 
-                            const letter = node.attributes.item(0).nodeValue
-                            const path = node.attributes.item(1).nodeValue
+                    const letters = this.text.split('')
 
+                    letters.forEach((letter, i) => {
+                        nodes.forEach(node => {
+                            if(node.attributes){
+    
+                                const nodeLetter = node.attributes.item(0).nodeValue
+                                const nodePath = node.attributes.item(1).nodeValue
 
-                            console.log(`${letter}: ${path}`)
-                        }
+                                if (nodeLetter == letter.toUpperCase()){
+
+                                    console.log(`${nodeLetter}: ${nodePath}`)
+                                    // const svg = document.createElement(svg)
+                                    var newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path')
+                                    newElement.setAttribute("d", nodePath); //Set path's data
+                                    newElement.setAttribute("style", `x: ${100 * i}px`)
+                                    var transformAttr = ' translateX(' + 100 * i + ')';
+                                    newElement.setAttribute('transform', transformAttr);
+                                    // newElement.style.stroke = "#000"; //Set stroke colour
+                                    // newElement.style.strokeWidth = "5px"; //Set stroke width
+                                    svg.appendChild(newElement);
+                                }
+    
+                            }
+                        });
+                        
                     });
+
+
+                    // console.log(letters)
+
+                    
                 })
             const parser = new DOMParser()
             
@@ -42,5 +69,9 @@ export default {
 }
 </script>
 <style scoped>
+
+.svg-area{
+    position: absolute;
+}
 
 </style>
