794ab78acd8e8b7b9f6664e388e8e3875ae17b52
Will <willmdbaker@gmail.com>
2022-03-21 18:45:45 +0000

    Fixed button spam

--- src/App.vue
+++ src/App.vue
@@ -136,6 +136,13 @@ export default {
     font-family: 'Roboto Mono', monospace;
     /* background: #D3D0CB; */
     color: var(--primary-color);
+      -webkit-touch-callout: none; /* iOS Safari */
+    -webkit-user-select: none; /* Safari */
+     -khtml-user-select: none; /* Konqueror HTML */
+       -moz-user-select: none; /* Old versions of Firefox */
+        -ms-user-select: none; /* Internet Explorer/Edge */
+            user-select: none; /* Non-prefixed version, currently
+                                  supported by Chrome, Edge, Opera and Firefox */
 }
 
 body {
